5b241320f6bce080bd9c600734c0951df8de92cc
mcdotjs <mirectm@gmail.com>
2024-08-25 22:24:24 +0200

    gitignore, constants

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
-main = print("Starting asteroids!")
+from constants import *
+main = print("Starting asteroids!", "\nScreen width: " +
+             str(SCREEN_WIDTH), "\nScreen height: " + str(SCREEN_HEIGHT))
 if __name__ == "__main__":
     main()
