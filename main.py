fb94dccb625e7efee2f61b9c6fde34ccc37d5079
mcdotjs <mirectm@gmail.com>
2024-08-26 21:58:38 +0200

    fps

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     pygame.display.set_caption("jkjhhujkijkjkjkj")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -12,6 +15,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
